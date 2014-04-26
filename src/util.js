b3702ebdd54e6a36c9157fdf991fe9e2b131f0a2
Casey Howard <caseyhoward@caseyhoward.com>
2014-04-26 11:10:43 -0700

    Refactor json parser

--- src/util.js
+++ src/util.js
@@ -1,22 +1,23 @@
 var Util = {};
-var JsonParser = function() {
-  this.parse = function(text, callback) {
-    var tryParse = function(text) {
-      try {
-        return JSON.parse(text);
-      } catch (e) {
-      }
-    };
-    var parsedJson = tryParse(text);
-    if (callback && parsedJson) {
-      return callback(parsedJson);
-    } else {
-      return parsedJson;
-    }
-  };
-};
 
 Util.jsonParser = function() {
+  var JsonParser = function() {
+    this.parse = function(text, callback) {
+      var tryParse = function(text) {
+        try {
+          return JSON.parse(text);
+        } catch (e) {
+        }
+      };
+      var parsedJson = tryParse(text);
+      if (callback && parsedJson) {
+        return callback(parsedJson);
+      } else {
+        return parsedJson;
+      }
+    };
+  };
+
   return new JsonParser();
 };
 
