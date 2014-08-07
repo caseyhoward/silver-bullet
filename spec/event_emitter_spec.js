25c84664f3f37982b18685fa2e3e903dccfe442f
Casey Howard <caseyhoward@caseyhoward.com>
2014-08-06 19:09:00 -0700

    Add tests for event emitter

---
+++ spec/event_emitter_spec.js
@@ -0,0 +1,24 @@
+var EventEmitter = require('../src/event_emitter');
+
+describe('EventEmitter', function() {
+  var eventEmitter;
+
+  beforeEach(function() {
+    eventEmitter = EventEmitter.create();
+  });
+
+  it.only('emits events to listeners', function() {
+    var cools = [];
+    eventEmitter.on('cool', function() {
+      cools.push(1);
+    });
+    eventEmitter.on('not cool', function() {
+      cools.push('not cool, man');
+    });
+    eventEmitter.on('cool', function() {
+      cools.push(2);
+    });
+    eventEmitter.emit('cool');
+    expect(cools).to.deep.equal([1, 2]);
+  });
+});
